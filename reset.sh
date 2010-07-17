de8d21d42bcc1df59134f74784c0c6a8f939dcd6
Ben Adida <ben.adida@childrens.harvard.edu>
2010-07-16 18:55:30 -0700

    added redland db to reset

--- reset.sh
+++ reset.sh
@@ -1,3 +1,5 @@
+dropdb -U smart redland
 dropdb -U smart smart
-createdb -U smart -O smart smart
+createdb -U smart -O smart --encoding='utf8' smart
+createdb -U smart -O smart --encoding='utf8' redland
 python manage.py syncdb
