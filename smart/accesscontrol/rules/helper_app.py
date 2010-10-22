059e04ff388925d420739ed2d14d8885a7a9c68b
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2010-10-22 12:38:23 -0700

    helper app permissions

---
+++ smart/accesscontrol/rules/helper_app.py
@@ -0,0 +1,13 @@
+"""
+Rules for PHAs, AccessTokens, ReqTokens
+"""
+
+from smart.views import *
+
+def grant(happ, permset):
+    """
+    grant the permissions of an account to this permset
+    """
+    permset.grant(get_first_record_tokens, None)
+    permset.grant(get_next_record_tokens, None)
+    permset.grant(get_record_tokens, None)
