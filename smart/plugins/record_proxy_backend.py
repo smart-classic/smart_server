09dea9986101bdc0127b4f86c24bce48fa3cac21
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-01-26 14:23:47 -0800

    Proxying plugin allows farming GET /records/... out to a proxy back-end

---
+++ smart/plugins/record_proxy_backend.py
@@ -0,0 +1,25 @@
+import re, RDF, uuid
+from django.conf import settings
+from smart.common.util import remap_node, parse_rdf, LookupType
+from smart.common.rdf_ontology import api_types, api_calls, ontology
+from smart.lib.utils import url_request
+from smart.models.rdf_rest_operations import *
+from smart.models.ontology_url_patterns import CallMapper, BasicCallMapper
+
+PROXY_BASE  = "http://sandbox-api.smartplatforms.org"
+
+def proxy_get(request, *args, **kwargs):
+    print "proxying request", request.path, args, kwargs
+    url = PROXY_BASE + request.path    
+    ret = url_request(url, "GET", {})
+    return rdf_response(ret)
+
+@CallMapper.register
+class RecordItemProxy(BasicCallMapper):
+    @property
+    def map_score(self):
+        cat = str(self.call.category)
+        if cat.startswith("record_item") and str(self.call.method)=="GET":
+            return 2
+        return 0
+    maps_to = staticmethod(proxy_get)
