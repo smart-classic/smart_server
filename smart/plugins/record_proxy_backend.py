0e156c5832f35db670b55980103d83fa331f2c55
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-02-14 13:48:03 -0800

    updated proxy to use maps_p instead of map_score

--- smart/plugins/record_proxy_backend.py
+++ smart/plugins/record_proxy_backend.py
@@ -16,10 +16,9 @@ def proxy_get(request, *args, **kwargs):
 
 @CallMapper.register
 class RecordItemProxy(BasicCallMapper):
-    @property
-    def map_score(self):
-        cat = str(self.call.category)
-        if cat.startswith("record_item") and str(self.call.method)=="GET":
-            return 2
-        return 0
     maps_to = staticmethod(proxy_get)
+    
+    @property
+    def maps_p(self):
+        cat = str(self.call.category)
+        return cat.startswith("record_item") and str(self.call.method)=="GET"
