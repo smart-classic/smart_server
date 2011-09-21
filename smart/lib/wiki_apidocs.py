7ca9568c69deca0904f25116d00cb262b3844bd8
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2011-09-20 18:51:34 -0700

    Ontology support for nested restrictions; NamedIndividuals

--- smart/lib/wiki_apidocs.py
+++ smart/lib/wiki_apidocs.py
@@ -10,18 +10,38 @@ def strip_smart(s):
 
 def type_start(t):
     name = type_name_string(t)
+    if (sp.Code in [x.uri for x in t.parents]):
+        name += " code"
+
     description = t.description
     example = t.example
     print "==%s RDF==\n"%name
 
-    if len(t.parent_classes) > 0:
+    if len(t.parents) > 0:
         print "%s is a subtype of and inherits properties from:"%type_name_string(t)
         parents = []
-        for p in t.parent_classes:
+        for p in sorted(t.parents, key=lambda x: type_name_string(x)):
             parents.append("[[#%s RDF| %s]] "%( type_name_string(p),type_name_string(p)))
         print ", ".join(parents)
         print "\n" 
     if description: print "%s"%description+"\n"
+
+    if t.equivalent_classes:
+        ec = filter(lambda x: x.one_of, t.equivalent_classes)
+        print "''Constrained to one of:'' \n<pre>"
+        for member in [x for c in ec for x in c.one_of]:
+            ts  = filter(lambda x: x != owl.NamedIndividual, member.type)
+            identifier = split_uri(member.uri)
+            system = str(member.uri).split(identifier)[0]
+            spcode = split_uri(str(t.uri))
+            print """<spcode:%s rdf:about="%s">
+    <rdf:type rdf:resource="http://smartplatforms.org/terms#Code" /> 
+    <dcterms:title>%s</dcterms:title>
+    <sp:system>%s</sp:system>
+    <dcterms:identifier>%s</dcterms:identifier> 
+</spcode:%s>"""%(spcode, str(member.uri), member.title, system, identifier, spcode)
+        print "</pre>"
+
     if example:
         print "<pre>%s</pre>\n"%example
 
@@ -45,11 +65,14 @@ def wiki_batch_start(batch):
 def type_name_string(t):
     if t.name:
         return str(t.name)
+    return split_uri(str(t.uri))
 
-    try: return str(t.uri).rsplit("#",1)[1]
+def split_uri(t):
+    try: 
+        return str(t).rsplit("#",1)[1]
     except:
         try: 
-            return str(t.uri).rsplit("/",1)[1]
+            return str(t).rsplit("/",1)[1]
         except: 
             return ""
     
@@ -72,15 +95,22 @@ def wiki_properties_for_type(t):
         desc = c.description
 
         if type(c) is OWL_ObjectProperty:
-            d = (c.to_class.description or "")
-            if len(d) > 1000:
-                d = d[:1000]+"..."
-            if len(d) > 0:
-                d = ": " + d
+            is_code = sp.Code in [p.uri for p in c.to_class.parents] and " code" or ""
+            targetname = type_name_string(c.to_class)+ is_code
 
-            desc = type_name_string(c.to_class) + "[[#%s RDF | (details...)]]"%(type_name_string(c.to_class)) + d
+            desc = "'''%s'''[[#%s RDF | (details...)]]"%(targetname, targetname)
+            further = filter(lambda x: isinstance(x.all_values_from, OWL_Restriction), c.restrictions)
+            for f in further:
+                p = split_uri(str(f.all_values_from.on_property))
+                pc = f.all_values_from.all_values_from
+                pc = type_name_string(pc)
+                desc += "\n''where'' '''"+ p +   "''' comes from '''%s''' [[#%s code RDF | (details...)]]"%(pc,pc)
+
         elif type(c) is OWL_DataProperty:
-            desc += (c.all_values_from and c.all_values_from.n3() or "string literal")
+            avf = filter(lambda x: x.all_values_from, c.restrictions)
+            if len(avf) >0: d = avf[0].all_values_from.uri.n3()
+            else: d = str(rdfs.Literal)
+            desc += " "+ d
         cardinality = cardinalities[c.cardinality_string]
 
         properties_row(name, c.uri.n3(), cardinality, desc)
@@ -103,21 +133,24 @@ def wiki_api_for_type(t, calls_for_t):
             last_description = str(call.description)
              
 main_types = []
-helper_types = []
 calls_to_document = copy.copy(api_calls)
 
 for t in api_types:
     if t.is_statement or len(t.calls) > 0:
         main_types.append(t)
-    elif (sp.Component in [x.uri for x in t.parent_classes] and 
-          sp.Code not in [x.uri for x in t.parent_classes]):
-        helper_types.append(t)
+    elif (sp.Component in [x.uri for x in t.parents]):
+        main_types.append(t)
 
 def type_sort_order(x): 
-    is_record = filter(lambda x: "record" in x, [c.category for c in x.calls])
-    if len(is_record) > 0 or len(x.calls) == 0:
-        return "Clinical Statement"
-    return "Container-level "
+    if x.is_statement or len(x.calls) > 0:
+        is_record = filter(lambda x: "record" in x, [c.category for c in x.calls])
+        if len(is_record) > 0 or len(x.calls) == 0:
+            return "Clinical Statement"
+        return "Container-level"
+    elif sp.Code in [p.uri for p in x.parents]:
+        return "Data code"
+    else:
+        return "Component"
 
 def call_category(x):
     return x.category.split("_")[0].capitalize()
@@ -131,7 +164,6 @@ def call_sort_order(x):
     return ret
 
 main_types = sorted(main_types, key=lambda x: type_sort_order(x) + str(x.name))
-helper_types = sorted(helper_types, key=lambda x: x.name)
 calls_to_document = sorted(calls_to_document, key=call_sort_order)
 
 import sys
@@ -144,10 +176,6 @@ if __name__=="__main__":
                 wiki_batch_start(current_batch+" Types") # e.g. "Record Items" or "Container Items"
             wiki_payload_for_type(t)
 
-        wiki_batch_start("Component Types") # e.g. "Record Items" or "Container Items"            
-        for t in helper_types: 
-            wiki_payload_for_type(t)            
-            
             
     if "api" in sys.argv:
         current_batch = None
@@ -162,4 +190,3 @@ if __name__=="__main__":
             calls_for_t = filter(lambda x: call_category(x)==current_batch, sorted(target.calls))
             processed.extend(calls_for_t)
             wiki_api_for_type(target, calls_for_t)
-
