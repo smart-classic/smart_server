e9aad89f97d655ac2cc291e60ef676967e810a39
Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
2012-04-18 17:42:58 -0400

    Fix kill_servers call in smart_manager for OS X

--- load_tools/smart_manager.py
+++ load_tools/smart_manager.py
@@ -258,6 +258,13 @@ def main():
         ui_server = server_settings.SMART_UI_SERVER_LOCATION
 
     if args.kill_servers:
+      if platform.system() == 'Darwin':
+        call_command("ps ax | "+
+                     "grep -i 'python' | "+
+                     "grep -i 'manage.py' | "+
+                     "egrep  -o '^[0-9]+' | "+
+                     "xargs -t  kill")
+      else:
         call_command("ps -ah | "+
                      "grep -i 'python' | "+
                      "grep -i 'manage.py' | "+
