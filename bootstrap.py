24915679acd265709f1dc7c6b5ef24f68f005410
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-04-25 02:06:21 -0400

    Escape special characters

--- bootstrap.py
+++ bootstrap.py
@@ -20,7 +20,7 @@ base_url = endpoint.replace(repo, "")
 
 if settings.TRIPLESTORE['engine'] == "sesame":
     s("""wget --post-data='context='  %s/clear -O /dev/null""" % endpoint)
-    s("""wget --post-data='type=native&Repository+ID='%s'&Repository+title=Record-level+RDF+by+context&Triple+indexes=spoc%2Cposc%2Ccspo'  %sNONE/create -O /dev/null""" % (repo, base_url))
+    s("""wget --post-data='type=native&Repository+ID='%s'&Repository+title=Record-level+RDF+by+context&Triple+indexes=spoc\%2Cposc\%2Ccspo'  %sNONE/create -O /dev/null""" % (repo, base_url))
 elif settings.TRIPLESTORE['engine'] == "stardog":
     s("""stardog-admin drop -n %s""" % repo)
     s("""stardog-admin create -n %s -t D -u admin -p admin --server snarl://localhost:5820""" % repo)
