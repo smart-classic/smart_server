0690758586c4213f936e4378f49cf05131bf3d22
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-05-30 10:23:34 -0400

    Adds configurable email notification to the record proxy plugin

--- smart/plugins/record_proxy_backend.py
+++ smart/plugins/record_proxy_backend.py
@@ -4,6 +4,8 @@ from smart.client.common.rdf_ontology import api_types, api_calls, ontology
 from smart.lib.utils import url_request, URLFetchException
 from smart.models.rdf_rest_operations import *
 from smart.models.ontology_url_patterns import CallMapper, BasicCallMapper
+import smtplib
+from email.mime.text import MIMEText
 
 def proxy_get(request, *args, **kwargs):
     print "proxying request", request.path, args, kwargs
@@ -12,7 +14,23 @@ def proxy_get(request, *args, **kwargs):
         ret = url_request(url, "GET", {})
         return rdf_response(ret)
     except URLFetchException as e:
-        return HttpResponse(content=e.body, status=e.status)
+    
+        if (settings.PROXY_ERROR_NOTIFICATION):
+            me = settings.PROXY_NOTIFICATION_FROM
+            you = settings.PROXY_NOTIFICATION_TO
+
+            msg = MIMEText(e.body)
+            msg['Subject'] = settings.PROXY_NOTIFICATION_SUBJECT
+            msg['From'] = me
+            msg['To'] = you
+
+            s = smtplib.SMTP(settings.PROXY_NOTIFICATION_SMTP_SERVER)
+            s.sendmail(me, [you], msg.as_string())
+            s.close()
+            
+            return HttpResponse(content=settings.PROXY_ERROR_MESSAGE_OVERRIDE, status=e.status)
+        else:
+            return HttpResponse(content=e.body, status=e.status)
 			
 
 @CallMapper.register
