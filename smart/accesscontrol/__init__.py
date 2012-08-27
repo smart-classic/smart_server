a25a7c5891bc8c592c82d3718ec71792997da482
Pascal Pfiffner <phase.of.matter@gmail.com>
2012-08-27 15:09:40 -0400

    pep8-ify

--- smart/accesscontrol/__init__.py
+++ smart/accesscontrol/__init__.py
@@ -2,73 +2,78 @@ import sys
 import smart
 #from smart import views
 
+
 class PermissionSet(object):
 
-  def __init__(self, principal):
-    self.grants   = {}
-    self.checkers = {}
-    self.request  = {}
-    self.principal    = principal
-    self.view_args    = ()
-    self.view_kwargs  = {}
+    def __init__(self, principal):
+        self.grants = {}
+        self.checkers = {}
+        self.request = {}
+        self.principal = principal
+        self.view_args = ()
+        self.view_kwargs = {}
 
-  def grant(self, view_func, parameter_callbacks=None):
-    self.grants[view_func] = parameter_callbacks
+    def grant(self, view_func, parameter_callbacks=None):
+        self.grants[view_func] = parameter_callbacks
 
-  def evaluate(self, request, view_func, view_args, view_kwargs):
-    """
-    a permission set is checked against a particular call.
-    some information in the parameters here is duplicated, i.e. view_func depends on request,
-    but it's provided here as convenience.
-    """
-    if not self.grants.has_key(view_func):
-      return False
+    def evaluate(self, request, view_func, view_args, view_kwargs):
+        """
+        Returns a tuple: (success, message)
 
-    callbacks = self.grants[view_func]
+        A permission set is checked against a particular call.
+        some information in the parameters here is duplicated, i.e. view_func
+        depends on request, but it's provided here as convenience.
+        """
 
-    # if callbacks is None, we're done, it's all good
-    if callbacks == None:
-      return True
+        if view_func not in self.grants:
+            return (False, 'You are not allowed to request %s' % request.META.get('PATH_INFO', view_func))
 
-    # otherwise, more constraints to check
+        callbacks = self.grants[view_func]
 
-    # the callbacks are checked, they must all be true to succeed
-    # this is not as powerful as the Indivo full-fledged system, but
-    # I'm waiting for that to be revamped before using it here in SMArt
-    for callback in callbacks:
-      if not callback(request, view_func, view_args, view_kwargs):
-        return False
+        # if callbacks is None, we're done, it's all good
+        if callbacks is None:
+            return (True, None)
 
-    return True
+        # otherwise, more constraints to check
+
+        # the callbacks are checked, they must all be true to succeed
+        # this is not as powerful as the Indivo full-fledged system, but
+        # I'm waiting for that to be revamped before using it here in SMArt
+        for callback in callbacks:
+            if not callback(request, view_func, view_args, view_kwargs):
+                return (False, None)        # TODO: Add error message
+
+        return (True, None)
 
 
 def get_accessrule(view_func):
-  return None
+    return None
+
 
 def nouser_permset():
-  permset = PermissionSet(None)
-  grant_baseline(permset)
-  return permset
+    permset = PermissionSet(None)
+    grant_baseline(permset)
+    return permset
+
 
 def grant_baseline(permset):
-  """Grant a common set of base grants"""
-  
-  # home
-  permset.grant(smart.views.home, None)
+    """Grant a common set of base grants"""
 
-  # list the phas
-  permset.grant(smart.views.all_phas, None)
+    # home
+    permset.grant(smart.views.home, None)
 
-  # manifest
-  permset.grant(smart.views.get_manifest, None)
+    # list the phas
+    permset.grant(smart.views.all_phas, None)
 
-  # static files
-  # for development purposes
-  import django.views.static
-  permset.grant(django.views.static.serve, None)
+    # manifest
+    permset.grant(smart.views.get_manifest, None)
 
-  # Anyone can make an OPTIONS request on a page
-  permset.grant(smart.views.allow_options, None)
-  permset.grant(smart.views.debug_oauth, None)
-  permset.grant(smart.views.smarthacks.get_container_manifest, None)
-  permset.grant(smart.views.smarthacks.download_ontology)
+    # static files for development purposes
+    import django.views.static
+    permset.grant(django.views.static.serve, None)
+
+    # Anyone can make an OPTIONS request on a page
+    permset.grant(smart.views.allow_options, None)
+    permset.grant(smart.views.debug_oauth, None)
+    permset.grant(smart.views.smarthacks.get_container_manifest, None)
+    permset.grant(smart.views.smarthacks.download_ontology)
--- smart/middlewares/authorization.py
+++ smart/middlewares/authorization.py
@@ -15,48 +15,60 @@ from django.conf import settings
 
 from smart import accesscontrol
 
+
 class Authorization(object):
+    """Authorization class to authorize incoming calls
+    """
 
-  def process_view(self, request, view_func, view_args, view_kwargs):
-    """ The process_view() hook allows us to examine the request before view_func is called"""
-    # Url exception(s)
-    
-    for exc_pattern in settings.SMART_ACCESS_CONTROL_EXCEPTIONS:
-      if re.match(exc_pattern, request.path):
-        return None
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """ The process_view() hook allows us to examine the request before
+        view_func is called
+        """
 
-    if hasattr(view_func, 'resolve'):
-      view_func = view_func.resolve(request)
+        # Bypass authorization check for some calls
+        for exc_pattern in settings.SMART_ACCESS_CONTROL_EXCEPTIONS:
+            if re.match(exc_pattern, request.path):
+                return None
 
-    try:
-      if view_func:
-        permission_set = self.get_permset(request)
+        if hasattr(view_func, 'resolve'):
+            view_func = view_func.resolve(request)
 
-        # given a set of permissions, and a rule for access checking
-        # apply the rules to the permission set with the current request parameters
-#        import rpdb2
-#        rpdb2.start_embedded_debugger("a")
-        if permission_set:
-          if permission_set.evaluate(request, view_func, view_args, view_kwargs):
-            print "And permitted for ", view_func.__name__, request.principal
-            return None
-          print "Permission denied for ", view_func.__name__, request.principal
+        denied_message = None
+        try:
+            if view_func:
+                permission_set = self.get_permset(request)
+
+                # given a set of permissions, and a rule for access checking
+                # apply the rules to the permission set with the current request
+                # parameters
+#                import rpdb2
+#                rpdb2.start_embedded_debugger("a")
+                if permission_set:
+                    success, message = permission_set.evaluate(request, view_func, view_args, view_kwargs)
+                    if success:
+                        print "And permitted for %s %s" % (view_func.__name__, request.principal)
+                        return None
+                    denied_message = message
+                    print "Permission denied for %s %s: %s" % (view_func.__name__, request.principal, message)
 
         # otherwise, this will fail
-    except:
-      print "Exception: Permission denied for ", view_func.__name__, request.principal
-      import sys, traceback
-      traceback.print_exc(file=sys.stderr)
-      raise PermissionDenied
-    raise PermissionDenied
+        except:
+            print "Exception: Permission denied for %s %s" % (view_func.__name__, request.principal)
+            import sys
+            import traceback
+            traceback.print_exc(file=sys.stderr)
+            raise PermissionDenied
 
-  def get_permset(self, request):
-    if request.principal:
-      "The permset is for", request.principal
-      return request.principal.permset
-    else:
-      "The permiset is for nouser"
-      return smart.accesscontrol.nouser_permset()
+        # TODO: Can we return something that actually shows the message? Stupid Django :P
+        raise PermissionDenied
+
+    def get_permset(self, request):
+        if request.principal:
+            "The permset is for", request.principal
+            return request.principal.permset
+        else:
+            "The permset is for nouser"
+            return smart.accesscontrol.nouser_permset()
 
 
 # Mark that the authorization module has been loaded
