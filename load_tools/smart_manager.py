d9bb9ad5442ece902e84b106705d15c5f8d44d50
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2013-10-02 20:59:32 +0300

    Merge in templatization from smart_ansible

--- load_tools/smart_manager.py
+++ load_tools/smart_manager.py
@@ -206,15 +206,15 @@ def main():
                             "smart_server/bootstrap_helpers/bootstrap_applications.py ")
 
         do_sed('smart_server/bootstrap_helpers/application_list.json', 
-                   'http:\/\/localhost:8001',
+                   'http:\/\/\{\{hostname\}\}:8001',
                    app_server_base_url)
                    
         do_sed('smart_server/bootstrap_helpers/bootstrap_applications.py', 
-                   'http:\/\/localhost:8001',
+                   'http:\/\/\{\{hostname\}\}:8001',
                    app_server_base_url)
                    
         do_sed('smart_server/bootstrap_helpers/bootstrap_applications.py', 
-                   'http:\/\/localhost:7001',
+                   'http:\/\/\{\{hostname\}\}:7001',
                    ui_server_base_url)
 
 
